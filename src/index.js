1306546a8f30cc86fc020448a41d931ad7558424
ddody <infinitylv@naver.com>
2018-05-30 13:36:45 +0900

    edit webfont

--- src/index.js
+++ src/index.js
@@ -4,9 +4,15 @@ import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
 import store, { history } from './store';
 import App from './containers/app';
-
+import WebFont from 'webfontloader';
 import 'sanitize.css/sanitize.css';
 import './index.css';
+
+WebFont.load({
+  google: {
+    families: ['notosanskr', 'Roboto:300,400,500', 'Material+Icons']
+  }
+});
 
 const target = document.querySelector('#root');
 
